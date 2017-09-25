1e0c486407ffc0225b61d1911fe5dcfec74ed6fd
Luciano Oliveira <luciano101@msn.com>
2017-09-24 20:43:21 -0700

    add id to each word

--- spec/words_spec.rb
+++ spec/words_spec.rb
@@ -29,4 +29,15 @@ describe(Word) do
       expect(Word.all()).to(eq([]))
     end
   end
+
+  describe("#id") do
+    it("increments an id by 1 each time a new item is added") do
+      word = Word.new({:word => 'obtuse', :definition => 'annoyingly insensitive or slow to understand.'})
+      word.save()
+      word2 = Word.new({:word => 'tsar', :definition => 'a person appointed by government to advise on and coordinate policy in a particular area'})
+      word2.save()
+      expect(word.id()).to(eq(1))
+      expect(word2.id()).to(eq(2))
+    end
+  end
 end
